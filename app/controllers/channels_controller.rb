256ff335ab61462193590e4a03462018b9c9c899
huutamnguyen <tamhnguyen333@gmail.com>
2017-08-24 17:04:25 +0700

    finish engine 04

--- app/controllers/channels_controller.rb
+++ app/controllers/channels_controller.rb
@@ -1,5 +1,6 @@
 class ChannelsController < ApplicationController
   before_action :set_channel, only: [:show, :edit, :update, :destroy]
+  skip_before_action :verify_authenticity_token
 
   # GET /channels
   # GET /channels.json
@@ -10,15 +11,18 @@ class ChannelsController < ApplicationController
   # GET /channels/1
   # GET /channels/1.json
   def show
+    @channel = Channel.find(params[:id])
+    render json: @channel
   end
 
   # GET /channels/new
   def new
-    @channel = Channel.new
+    @channel = Channelel.new
   end
 
   # GET /channels/1/edit
   def edit
+    @channel = Channel.find(params[:id])
   end
 
   # POST /channels
@@ -28,10 +32,10 @@ class ChannelsController < ApplicationController
 
     respond_to do |format|
       if @channel.save
-        format.html { redirect_to @channel, notice: 'Channel was successfully created.' }
+        #format.html { redirect_to @channel, notice: 'Channel was successfully created.' }
         format.json { render :show, status: :created, location: @channel }
       else
-        format.html { render :new }
+        #format.html { render :new }
         format.json { render json: @channel.errors, status: :unprocessable_entity }
       end
     end
@@ -42,10 +46,10 @@ class ChannelsController < ApplicationController
   def update
     respond_to do |format|
       if @channel.update(channel_params)
-        format.html { redirect_to @channel, notice: 'Channel was successfully updated.' }
+        #format.html { redirect_to @channel, notice: 'Channel was successfully updated.' }
         format.json { render :show, status: :ok, location: @channel }
       else
-        format.html { render :edit }
+        #format.html { render :edit }
         format.json { render json: @channel.errors, status: :unprocessable_entity }
       end
     end
@@ -56,19 +60,19 @@ class ChannelsController < ApplicationController
   def destroy
     @channel.destroy
     respond_to do |format|
-      format.html { redirect_to channels_url, notice: 'Channel was successfully destroyed.' }
-      format.json { head :no_content }
+      #format.html { redirect_to channels_url, notice: 'Channel was successfully destroyed.' }
+      format.json { render json: {"status":"sucessfull"} }
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_channel
-      @channel = Channel.find(params[:id])
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_channel
+    @channel = Channel.find(params[:id])
+  end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def channel_params
-      params.fetch(:channel, {})
-    end
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def channel_params
+    params.require(:channel).permit(:name, :owner, :org_id, :is_private, :state, :status)
+  end
 end
--- app/views/channels/_channel.json.jbuilder
+++ app/views/channels/_channel.json.jbuilder
@@ -1,2 +1,2 @@
-json.extract! channel, :id, :created_at, :updated_at
-json.url channel_url(channel, format: :json)
+json.extract! channel, :id, :name, :owner, :org_id, :is_private, :state, :status
+#json.url channel_url(channel, format: :json)
