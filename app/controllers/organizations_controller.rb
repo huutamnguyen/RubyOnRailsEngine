d2c7743c6fcdbe4364f93ed20605b9a3d6a49d9b
huutamnguyen <tamhnguyen333@gmail.com>
2017-08-24 16:30:47 +0700

    finish engine 03

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -1,5 +1,6 @@
 class OrganizationsController < ApplicationController
   before_action :set_organization, only: [:show, :edit, :update, :destroy]
+  skip_before_action :verify_authenticity_token
 
   # GET /organizations
   # GET /organizations.json
@@ -10,6 +11,8 @@ class OrganizationsController < ApplicationController
   # GET /organizations/1
   # GET /organizations/1.json
   def show
+    @organization = Organization.find(params[:id])
+    render json: @organization
   end
 
   # GET /organizations/new
@@ -19,6 +22,7 @@ class OrganizationsController < ApplicationController
 
   # GET /organizations/1/edit
   def edit
+    @organization = Organization.find(params[:id])
   end
 
   # POST /organizations
@@ -28,10 +32,10 @@ class OrganizationsController < ApplicationController
 
     respond_to do |format|
       if @organization.save
-        format.html { redirect_to @organization, notice: 'Organization was successfully created.' }
+        #format.html { redirect_to @organization, notice: 'Organization was successfully created.' }
         format.json { render :show, status: :created, location: @organization }
       else
-        format.html { render :new }
+        #format.html { render :new }
         format.json { render json: @organization.errors, status: :unprocessable_entity }
       end
     end
@@ -42,10 +46,10 @@ class OrganizationsController < ApplicationController
   def update
     respond_to do |format|
       if @organization.update(organization_params)
-        format.html { redirect_to @organization, notice: 'Organization was successfully updated.' }
+        #format.html { redirect_to @organization, notice: 'Organization was successfully updated.' }
         format.json { render :show, status: :ok, location: @organization }
       else
-        format.html { render :edit }
+        #format.html { render :edit }
         format.json { render json: @organization.errors, status: :unprocessable_entity }
       end
     end
@@ -56,19 +60,19 @@ class OrganizationsController < ApplicationController
   def destroy
     @organization.destroy
     respond_to do |format|
-      format.html { redirect_to organizations_url, notice: 'Organization was successfully destroyed.' }
-      format.json { head :no_content }
+      #format.html { redirect_to organizations_url, notice: 'Organization was successfully destroyed.' }
+      format.json { render json: {"status":"sucessfull"} }
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_organization
-      @organization = Organization.find(params[:id])
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_organization
+    @organization = Organization.find(params[:id])
+  end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def organization_params
-      params.fetch(:organization, {})
-    end
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def organization_params
+    params.require(:organization).permit(:name, :logo, :primary_contact_email, :status, :details)
+  end
 end
